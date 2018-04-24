800661cd403fee9535a7bfc67604970f16c5f394
zayrtun <zay.t@hotmail.com>
2018-04-24 21:50:11 +0630

    0301

--- public/index.php
+++ public/index.php
@@ -2,6 +2,29 @@
 
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
+<?php 
+
+  if (isset($_GET["id"])) {
+    $page_id = $_GET["id"];
+    $page = find_page_by_id($page_id);
+    if(!$page) {
+      redirect_to(url_for('/index.php'));
+    }
+    $subject_id = $page['subject_id'];
+  } elseif (isset($_GET["subject_id"])) {
+    $subject_id = $_GET["subject_id"];
+    $page_set = find_pages_by_subject_id($subject_id);
+    $page = mysqli_fetch_assoc($page_set);
+    mysqli_free_result($page_set);
+    if(!$page) {
+      redirect_to(url_for('/index.php'));
+    }
+    $page_id = $page['id'];
+  } else {
+    //nothing selected show the home page
+  }
+
+?>
 <div id="main">
 
   <?php include(SHARED_PATH . '/public_navigation.php'); ?>
@@ -9,12 +32,19 @@
   <div id="page">
     
     <?php 
-      //Show the homepage
-      //The homepage content could :
-      // * be static content (here or in a shared file)
-      // * show the first page from the nav
-      // * be in the database but add code to hide in the nav
-      include(SHARED_PATH . '/static_homepage.php');
+      if(isset($page)) {
+        //show the page from the database
+        //TODO add html escaping back in
+        // echo h($page['content']);
+        echo $page['content'];
+      } else {
+        //Show the homepage
+        //The homepage content could :
+        // * be static content (here or in a shared file)
+        // * show the first page from the nav
+        // * be in the database but add code to hide in the nav
+        include(SHARED_PATH . '/static_homepage.php');
+      }
     ?>
 
   </div>
