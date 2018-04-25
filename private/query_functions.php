4b2b0f81de605cd97437bbdc0901096db7e44ac1
zayrtun <zay.t@hotmail.com>
2018-04-25 14:12:19 +0630

    0405

--- private/query_functions.php
+++ private/query_functions.php
@@ -1,20 +1,30 @@
 <?php 
 
-function find_all_subjects() {
+function find_all_subjects($options=[]) {
   global $db;
 
+  $visible = $options['visible'] ?? false;
+
   $sql = "SELECT * FROM subjects ";
+  if($visible) {
+    $sql .= "WHERE visible = true ";
+  }
   $sql .= "ORDER BY position ASC";
   $result = mysqli_query($db, $sql);
   confirm_result_set($result);
   return $result;
 }
 
-function find_subject_by_id($id) {
+function find_subject_by_id($id, $options=[]) {
   global $db;
 
+  $visible = $options['visible'] ?? false;
+
   $sql = "SELECT * FROM subjects ";
-  $sql .= "WHERE id='" . db_scape($db, $id) . "'";
+  $sql .= "WHERE id='" . db_scape($db, $id) . "' ";
+  if($visible) {
+    $sql .= "AND visible = true";
+  }
   $result = mysqli_query($db, $sql);
   confirm_result_set($result);
   $subject = mysqli_fetch_assoc($result);
@@ -135,11 +145,16 @@ function find_all_pages() {
   return $result;
 }
 
-function find_page_by_id($id) {
+function find_page_by_id($id, $options=[]) {
   global $db;
+
+  $visible = $options['visible'] ?? false;
   
   $sql = "SELECT * FROM pages ";
-  $sql .= "WHERE id='" . db_scape($db, $id) ."'";
+  $sql .= "WHERE id='" . db_scape($db, $id) ."' ";
+  if($visible) {
+    $sql .= "AND visible = true";
+  }
   $result = mysqli_query($db, $sql);
   confirm_result_set($result);
   $page = mysqli_fetch_assoc($result);
@@ -265,11 +280,16 @@ function delete_page($id) {
   }
 }
 
-function find_pages_by_subject_id($subject_id) {
+function find_pages_by_subject_id($subject_id, $options=[]) {
   global $db;
+
+  $visible = $options['visible'] ?? false;
   
   $sql = "SELECT * FROM pages ";
   $sql .= "WHERE subject_id='" . db_scape($db, $subject_id) ."' ";
+  if($visible) {
+    $sql .= "AND visible = true ";
+  }
   $sql .= "ORDER BY position ASC";
   $result = mysqli_query($db, $sql);
   confirm_result_set($result);
--- public/index.php
+++ public/index.php
@@ -4,16 +4,31 @@
 
 <?php 
 
+$preview = false;
+if(isset($_GET['preview'])) {
+  $preview = $_GET["preview"] == 'true' ? true : false;
+}
+$visible = !$preview;
+
   if (isset($_GET["id"])) {
     $page_id = $_GET["id"];
-    $page = find_page_by_id($page_id);
+    $page = find_page_by_id($page_id, ['visible' => $visible]);
     if(!$page) {
       redirect_to(url_for('/index.php'));
     }
     $subject_id = $page['subject_id'];
+    $subject = find_subject_by_id($subject_id, ['visible' => $visible]);
+    if(!$subject) {
+      redirect_to(url_for('/index.php'));
+    }
+
   } elseif (isset($_GET["subject_id"])) {
     $subject_id = $_GET["subject_id"];
-    $page_set = find_pages_by_subject_id($subject_id);
+    $subject = find_subject_by_id($subject_id, ['visible' => $visible]);
+    if(!$subject) {
+      redirect_to(url_for('/index.php'));
+    }
+    $page_set = find_pages_by_subject_id($subject_id, ['visible' => $visible]);
     $page = mysqli_fetch_assoc($page_set);
     mysqli_free_result($page_set);
     if(!$page) {
@@ -35,8 +50,8 @@
       if(isset($page)) {
         //show the page from the database
         //TODO add html escaping back in
-        // echo h($page['content']);
-        echo $page['content'];
+        $allowed_tags = '<div><img><h1><h2><p><br><strong><em><ul><li>';
+        echo strip_tags($page['content'], $allowed_tags);
       } else {
         //Show the homepage
         //The homepage content could :
